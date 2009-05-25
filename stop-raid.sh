1825d4a2b2ad27e95a6f2c5661037f9e91012f88
Olof-Joachim Frahm <Olof.Frahm@web.de>
2009-05-25 13:45:31 +0200

    Changed number of disks.

--- stop-raid.sh
+++ stop-raid.sh
@@ -4,9 +4,7 @@ MDADM=/home/rudolf/src/mdadm-2.6.8/mdadm
 
 #$MDADM --manage /dev/md0 -S
 
-for i in 0 1 2 3 4
+for i in 0 1 2 3 4 5
 do
-    if [[ ! -e /dev/loop$i ]]; then
-	losetup -d /dev/loop$i
-    fi
+    losetup -d /dev/loop$i
 done
